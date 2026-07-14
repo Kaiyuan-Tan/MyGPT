645c228f913ee83433a9391e57ea72febb48a3ae
Kaiyuan-Tan <727353680@qq.com>
2026-07-14 11:38:44 -0700

    text generation

--- DummyGPTModel.py
+++ DummyGPTModel.py
@@ -37,19 +37,6 @@ class DummyLayerNorm(nn.Module): #E
     def forward(self, x):
         return x
 
-class LayerNorm(nn.Module):
-    def __init__(self, emb_dim):
-        super().__init__()
-        self.eps = 1e-5
-        self.scale = nn.Parameter(torch.ones(emb_dim))
-        self.shift = nn.Parameter(torch.zeros(emb_dim))
- 
-    def forward(self, x):
-        mean = x.mean(dim=-1, keepdim=True)
-        var = x.var(dim=-1, keepdim=True, unbiased=False)
-        norm = (x - mean) / torch.sqrt(var + self.eps)
-        return self.scale * norm + self.shift
-
 # tokenizer = tiktoken.get_encoding("gpt2")
 # batch = []
 # txt1 = "Every effort moves you"
--- test.py
+++ test.py
@@ -5,15 +5,31 @@ from model.GELU import GELU
 from model.residual import ExampleDeepNeuralNetwork
 from config.GPT_CONFIG_124M import GPT_CONFIG_124M
 from model.block import TransformerBlock
+import tiktoken
+from model.GPTModel import GPTModel
+from text_generation import generate_text_simple
 
 # ffn = FeedForward(GPT_CONFIG_124M)
 # x = torch.rand(2, 3, 768)
 # out = ffn(x)
 # print(out.shape)
 
-layer_sizes = [3, 3, 3, 3, 3, 1] 
-sample_input = torch.tensor([[1., 0., -1.]])
-torch.manual_seed(123)
+# layer_sizes = [3, 3, 3, 3, 3, 1] 
+# sample_input = torch.tensor([[1., 0., -1.]])
+
+##################################################
+
+tokenizer = tiktoken.get_encoding("gpt2")
+# batch = []
+# txt1 = "Every effort moves you"
+# txt2 = "Every day holds a"
+# batch.append(torch.tensor(tokenizer.encode(txt1)))
+# batch.append(torch.tensor(tokenizer.encode(txt2)))
+# batch = torch.stack(batch, dim=0)
+
+##################################################
+
+# torch.manual_seed(123)
 # model1 = ExampleDeepNeuralNetwork(layer_sizes, use_shortcut=False)
 # model2 = ExampleDeepNeuralNetwork(layer_sizes, use_shortcut=True)
 
@@ -38,8 +54,34 @@ def print_gradients(model, x):
 
 # print_gradients(model2, sample_input)
 
-x = torch.rand(2, 4, 768)  #A
-block = TransformerBlock(GPT_CONFIG_124M)
-output = block(x)
-print("Input shape:", x.shape)
-print("Output shape:", output.shape)
+# x = torch.rand(2, 4, 768)  #A
+# block = TransformerBlock(GPT_CONFIG_124M)
+# output = block(x)
+# print("Input shape:", x.shape)
+# print("Output shape:", output.shape)
+
+
+model = GPTModel(cfg=GPT_CONFIG_124M)
+# total_params = sum(p.numel() for p in model.parameters())
+# print(f"Total number of parameters: {total_params:,}")
+# print("Token embedding layer shape:", model.tok_emb.weight.shape)
+# print("Output layer shape:", model.out_head.weight.shape)
+# total_params_gpt2 =  total_params - sum(p.numel() for p in model.out_head.parameters())
+# print(f"Number of trainable parameters considering weight tying: {total_params_gpt2:,}")
+# total_size_bytes = total_params * 4  #A
+# total_size_mb = total_size_bytes / (1024 * 1024)  #B
+# print(f"Total size of the model: {total_size_mb:.2f} MB")
+
+start_context = "Hello, I am"
+encoded = tokenizer.encode(start_context)
+encoded_tensor = torch.tensor(encoded).unsqueeze(0) #A
+
+model.eval()
+out = generate_text_simple(
+    model = model,
+    idx = encoded_tensor,
+    max_new_tokens = 6,
+    context_size=GPT_CONFIG_124M["context_length"]
+)
+decoded_text = tokenizer.decode(out.squeeze(0).tolist())
+print(decoded_text)
